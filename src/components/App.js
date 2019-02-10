2b6bf6832a2ed85e86b0382da4bd56f1f1e7fdfb
Harsh <harsh.makadia@marutitech.com>
2019-02-10 23:21:21 +0530

    feat: initial setup of react app with webpack

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Yay! React App with Webpack :p</h1>
+        <span><i>GoodBye <a href="https://github.com/facebook/create-react-app">CRA</a> you will be missed</i></span>
+      </div>
+    );
+  }
+}
+
+export default App;
